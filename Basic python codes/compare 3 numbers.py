ace0abcba75eb98769f8fce8a92e4d474ffd134b
aarush <rohitsingh7802@gmail.com>
2026-08-03 16:02:53 +0530

    Basic python codes

---
+++ Basic python codes/compare 3 numbers.py
@@ -0,0 +1,5 @@
+n=int(input("Enter first number:"))
+if n%7==0:
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
---
+++ Basic python codes/sum of two.py
@@ -0,0 +1,4 @@
+num_1=int(input("Eneter the first number"))
+num_2=int(input("Enter the second number"))
+sum=num_1+num_2
+print("The sum of",num_1,"and",num_2,"is",sum)
