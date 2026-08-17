e4ecb58eb01ec809e96295cb232c2a7742238264
aarush <rohitsingh7802@gmail.com>
2026-08-17 11:46:13 +0530

    slicing

---
+++ Coding of AI BENNETT/slice.py
@@ -0,0 +1,20 @@
+list_1=[1,2,3,4,5,6]
+print(list_1[0:5:2])
+
+letters=['a','b','c','d','e']
+
+print(letters[1:3])
+print(len(letters[1:3]))
+
+word = 'PYTHON'
+print(word[::-1])
+print(word[::2])
+
+
+
+
+data=[5,10,15,20,25,30,35,40]
+print(data[:3])
+print(data[5:8])
+print(data[::3])
+
